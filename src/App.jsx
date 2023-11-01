ec71410ad2c53bfcbec8e2e50c87b8586b34c5e4
Carolina-Cardenas <carolina.beatriz.cardenas@gmail.com>
2023-11-01 21:15:38 +0100

    show messages

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,20 @@ import { useState,useEffect } from "react";
 import { Send } from "./components/Send"; 
 import { Show } from "./components/Show";
 export const App = () => {
-  
 const [messages, setMessages] = useState([])
 useEffect(() => {
   fetchMessages()
 
+
 },[])
 
-const fetchMessages = async() => {
+  const fetchMessages = async() => {
   const url ="https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts"
   const messagesResponse =  await fetch(url);
   const messagesData = await messagesResponse.json();
 
   setMessages(messagesData);
-  
+
 }
  const renderMessages = () => {
   return messages.map(
@@ -26,13 +26,17 @@ const fetchMessages = async() => {
     }
   );
 }
-const renderContent = renderMessages();
- 
- return <div className="main-wrapper" >
-            <Send/>
-            <div className="list-wrapper">
-            {renderContent}
-            </div>
-            
-          </div>;
+ const renderContent = renderMessages() 
+
+ return (
+   <div className="main-wrapper">
+     <Send />
+     <div className="list-wrapper">
+       {renderContent}
+     </div>
+   </div>
+ );
 };
+
+
+
