f4d1ece146c10d3b0e793023f5b719e5fe47f4f0
Carolina-Cardenas <carolina.beatriz.cardenas@gmail.com>
2023-11-01 07:01:43 +0100

    Structure ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,38 @@
+import { useState,useEffect } from "react";
+import { Send } from "./components/Send"; 
+import { Show } from "./components/Show";
 export const App = () => {
-  return <div>Find me in src/app.jsx!</div>;
+  
+const [messages, setMessages] = useState([])
+useEffect(() => {
+  fetchMessages()
+
+},[])
+
+const fetchMessages = async() => {
+  const url ="https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts"
+  const messagesResponse =  await fetch(url);
+  const messagesData = await messagesResponse.json();
+
+  setMessages(messagesData);
+  
+}
+ const renderMessages = () => {
+  return messages.map(
+    ({ _id, createdAt, hearts, message }) => {
+      return(
+        <Show key={_id} createdAt={createdAt} hearts={hearts} message={message}/>
+      )
+    }
+  );
+}
+const renderContent = renderMessages();
+ 
+ return <div className="main-wrapper" >
+            <Send/>
+            <div className="list-wrapper">
+            {renderContent}
+            </div>
+            
+          </div>;
 };
