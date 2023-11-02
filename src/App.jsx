9258dc184a8da8c7a2f313717ccb56c0ac84503f
Carolina-Cardenas <carolina.beatriz.cardenas@gmail.com>
2023-11-02 22:04:03 +0100

    fix message

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,42 @@ import { Send } from "./components/Send";
 import { Show } from "./components/Show";
 export const App = () => {
 const [messages, setMessages] = useState([])
+const [newMessage, setNewMessage] = useState ("")
+const url ="https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts"
+
 useEffect(() => {
   fetchMessages()
-
-
 },[])
 
+
   const fetchMessages = async() => {
-  const url ="https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts"
+    alert("in fetch")
   const messagesResponse =  await fetch(url);
   const messagesData = await messagesResponse.json();
 
   setMessages(messagesData);
+}
+
+const sendMessage = async(message) => {
+ alert(newMessage)
+ const response = await fetch(url , {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify({ message: newMessage }),
+});
+alert(response.status)
+if (response.status === 201) {
+  // Mensaje enviado con éxito, puedes actualizar la lista de mensajes
+  // o realizar alguna otra acción aquí
+  alert(message)
+  fetchMessages();
+  setNewMessage("");
+} else {
+  // Manejar errores
+  console.error("Error al enviar el mensaje");
+}
 
 }
  const renderMessages = () => {
@@ -30,7 +54,7 @@ useEffect(() => {
 
  return (
    <div className="main-wrapper">
-     <Send />
+      <Send updateNewMessage={setNewMessage} sendMessage={sendMessage} />
      <div className="list-wrapper">
        {renderContent}
      </div>
