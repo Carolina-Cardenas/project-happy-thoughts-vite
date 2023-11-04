439a9d3dd1acaa3e409614be4c174c5d9a44d9af
Carolina-Cardenas <carolina.beatriz.cardenas@gmail.com>
2023-11-04 22:21:53 +0100

    finish

--- src/components/Show.jsx
+++ src/components/Show.jsx
@@ -1,13 +1,11 @@
-export const Show = ({ createdAt, hearts, message}) => {
-
+export const Show = ({ _id, createdAt, hearts, message, handleLike }) => {
   function timeAgo(fromTime) {
-    
-    const createdAt = new Date(fromTime)
+    const createdAt = new Date(fromTime);
     const currentTime = new Date();
     const timeDifference = currentTime - createdAt;
-  
+
     const minutesAgo = Math.floor(timeDifference / (1000 * 60));
-    
+
     if (minutesAgo < 1) {
       return "Just now";
     } else if (minutesAgo === 1) {
@@ -17,39 +15,34 @@ export const Show = ({ createdAt, hearts, message}) => {
     } else {
       const hoursAgo = Math.floor(minutesAgo / 60);
       if (hoursAgo === 1) {
-      return "1 hour ago";
+        return "1 hour ago";
       } else {
         return `${hoursAgo} hours ago`;
       }
     }
   }
-  
-  
-   const timeAgoString = timeAgo(createdAt);
-  
-  
+
+  const timeAgoString = timeAgo(createdAt);
+
   return (
-    <div className="message">
-    <p>
-      {message}
-    </p>
-    <div className="info-wrapper">
-      <div className="info-like">
-        <button type="button" id="likeBtn" className="like-button liked">
-          <span className="emoji" aria-label="like button">
-            ❤️
-          </span>
-        </button>
-        <span className="num-likes">
-          x {hearts}
-        </span>
+    <div id={_id} className="message">
+      <p>{message}</p>
+      <div className="info-wrapper">
+        <div className="info-like">
+          <button
+            type="button"
+            className={`like-button ${hearts > 0 ? "liked" : ""}`}
+            onClick={() => handleLike(_id)}
+          >
+            <span className="emoji" aria-label="like button">
+              ❤️
+            </span>
+          </button>
+          <span className="num-likes">x {hearts}</span>
+        </div>
+        <div className="info-time">{timeAgoString}</div>
+        <div id="container"></div>
       </div>
-      <div className="info-time">
-         {timeAgoString}
-      </div>
-      <div id="container"></div>
     </div>
-  </div>
-  
-  )
-}
+  );
+};
